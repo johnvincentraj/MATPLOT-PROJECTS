466f6504694eddd8f1f7a7268c8ae50e80e584f3
johnvincentraj <johnvincentraj078@gmail.com>
2021-08-10 15:09:04 +0530

    added graph with markers

--- default x points.py
+++ default x points.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # if x values is not given it take default
 # values as 1,2,3,4.,
-ypoints = ([3,24,34,54,76])
+ypoints = np.array([3,24,34,54,76])
 
 plt.plot(ypoints)
 plt.show()
