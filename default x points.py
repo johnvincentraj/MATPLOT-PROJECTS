6245a2760c694b8ff83dddcd36388be085c8020e
johnvincentraj <johnvincentraj078@gmail.com>
2021-08-10 15:04:55 +0530

    add default x points

---
+++ default x points.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# if x values is not given it take default
+# values as 1,2,3,4.,
+ypoints = ([3,24,34,54,76])
+
+plt.plot(ypoints)
+plt.show()
