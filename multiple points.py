7a953b90c1af3485ad064ebafc550c15799e4fdb
johnvincentraj <johnvincentraj078@gmail.com>
2021-08-10 15:09:44 +0530

    added np.array

--- multiple points.py
+++ multiple points.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-xpoints = ([0,4,8,13])
-ypoints = ([2,8,4,15])
+xpoints = np.array([0,4,8,13])
+ypoints = np.array([2,8,4,15])
 
 plt.plot(xpoints, ypoints)
 plt.show()
